c33e9ae8cdd5d74068285817d3c3bbb1dcf98932
Diego Machado <machadodiegoh@gmail.com>
2020-07-28 00:57:32 -0300

    added logic to list, add and remove items from backend

--- src/App.js
+++ src/App.js
@@ -1,26 +1,45 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import api from "./services/api";
 
 import "./styles.css";
 
 function App() {
+  const [data, setData] = useState([]);
+
+  useEffect(() => {
+    api
+      .get("/repositories")
+      .then((res) => setData(res.data))
+      .catch((err) => console.log(err.message));
+  }, []);
+
   async function handleAddRepository() {
-    // TODO
+    const newData = {
+      title: "React Repo",
+      url: "https://reactjs.org/",
+      techs: ["React", "React Native"],
+    };
+    const response = await api.post("/repositories", newData);
+    setData([...data, response.data]);
   }
 
   async function handleRemoveRepository(id) {
-    // TODO
+    api.delete(`/repositories/${id}`);
+    setData(data.filter((item) => item.id !== id));
   }
 
   return (
     <div>
       <ul data-testid="repository-list">
-        <li>
-          Repositório 1
-
-          <button onClick={() => handleRemoveRepository(1)}>
-            Remover
-          </button>
-        </li>
+        {data.length > 0 &&
+          data.map((item) => (
+            <li key={item.id}>
+              {item.title}
+              <button onClick={() => handleRemoveRepository(item.id)}>
+                Remover
+              </button>
+            </li>
+          ))}
       </ul>
 
       <button onClick={handleAddRepository}>Adicionar</button>
